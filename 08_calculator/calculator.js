eda344b22769d0c1ee5ba5b3f4ee406c06fbb5a9
Jordan Pacho <trainee.jordan.pacho@sigmalabs.co.uk>
2022-12-13 17:58:12 +0000

    Added functionality for all operators

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,23 +3,36 @@ const add = function(num1, num2) {
 };
 
 const subtract = function(num1, num2) {
-	
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce(
+    (accumulator, currentVal) => accumulator + currentVal, 0
+  )
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce(
+    (accumulator, currentVal) => accumulator * currentVal, 1
+  )
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+	return num ** power
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let value = 1
+  if (num == 1 || num === 1) {
+    return 1
+  } else {
+    for (let i = num; i > 1; i--) {
+      value *= i
+    }
+
+    return value
+  }
 };
 
 // Do not edit below this line
